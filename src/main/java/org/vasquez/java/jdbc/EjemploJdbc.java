d0613ca975da156856f4d68b428a1bafb448ff84
Peito1102 <renzitiwin@gmail.com>
2024-09-11 23:11:47 -0500

    primer commit

---
+++ src/main/java/org/vasquez/java/jdbc/EjemploJdbc.java
@@ -0,0 +1,25 @@
+package org.vasquez.java.jdbc;
+
+import java.sql.*;
+
+public class EjemploJdbc {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=UTC";
+        String username = "root";
+        String password = "mysql";
+        try {
+            Connection conn = DriverManager.getConnection(url,username,password);
+            Statement stmt = conn.createStatement();
+            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
+
+            while (resultado.next()) {
+                System.out.println(resultado.getString(2));
+            }
+            resultado.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
